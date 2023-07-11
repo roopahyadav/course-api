ad431f1e9510bd539bde6549872f1850d9572606
Roopa H <roopa.h@sap.com>
2023-07-11 23:23:42 +0530

    JDBC connection to H2 database

--- src/main/java/io/javabrains/springbootuickstart/security/SecurityConfiguration.java
+++ src/main/java/io/javabrains/springbootuickstart/security/SecurityConfiguration.java
@@ -1,38 +1,88 @@
 package io.javabrains.springbootuickstart.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /* In memory authentication */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("blah")
+//                .password("blah")
+//                .roles("USER")
+//                .and()
+//                .withUser("foo")
+//                .password("foo")
+//                .roles("ADMIN");
+//    }
+
+    //##########################################################################
+    // JDBC config
+
+    @Autowired
+    DataSource dataSource;
+
+    /* Default schema with fixed fixed users
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("pass")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
     }
+
+     */
+
+    /* Authenticating based on the user available in database. database is create in resources schema.sql */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled"+
+                        "from users" +
+                        "where username = ?")
+                .authoritiesByUsernameQuery(
+                        "select username,authority"+
+                                "from authorities" +
+                                "where username = ?"
+                );
+
+    }
+
+    /* INFO: If we have to connect to external data base then we have configure data
+    source url and credentials in application properties */
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-      return   NoOpPasswordEncoder.getInstance();
+        return   NoOpPasswordEncoder.getInstance();
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
-               .antMatchers("/admin","/*").hasRole("ADMIN")
-               .antMatchers("/user").hasRole("USER")
+               .antMatchers("/admin").hasRole("ADMIN")
+               .antMatchers("/user").hasAnyRole("USER","ADMIN")
                .antMatchers("/").permitAll()
                .and().formLogin();
     }
